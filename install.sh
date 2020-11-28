43d5281b6f7d91ebe79313ad3f8f6ebe74f788a2
YZero <yacomelian@gmail.com>
2020-11-28 14:19:25 +0000

    Version 20201128-1419 Testing on Pi Zero

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+./scripts/initialize_gpio.sh
+
+apt install git python3 python3-pip
+apt install git python3-rpi.gpio python3-serial python3-yaml
+pip3 install -r requirements.txt
+
+
---
+++ scripts/initialze_gpio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+echo "4" > /sys/class/gpio/export
+sleep 0.1
+echo "out" > /sys/class/gpio/gpio4/direction
+echo "0" > /sys/class/gpio/gpio4/value
