03daec7f73388b052f0c06be258e21b40e15fe22
yacomelian <yacomelian@gmail.com>
2020-12-07 22:26:35 +0000

    v20201207-2226 Added service files

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ apt install git python3 python3-pip
 apt install git python3-rpi.gpio python3-serial python3-yaml
 pip3 install -r requirements.txt
 
+sudo cp scripts/scripts/runsensor.service /etc/systemd/system/runsensor.service
 
